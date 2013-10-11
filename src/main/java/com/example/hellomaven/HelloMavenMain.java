7a55822dbaca0c71e8f9798af4ee64d33eb66c58
blackbernblack <hqyejbfe@sharklasers.com>
2013-10-11 15:20:27 +0300

    kyk

---
+++ src/main/java/com/example/hellomaven/HelloMavenMain.java
@@ -0,0 +1,13 @@
+package com.example.hellomaven;
+
+public class HelloMavenMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+	    System.out.println("HELLLLLLLLLooooo");
+
+	}
+
+}
