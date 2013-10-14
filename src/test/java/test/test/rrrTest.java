28aa0964f8054aa1867198a773b18bdd632ec3c9
blackbernblack <hqyejbfe@sharklasers.com>
2013-10-14 17:54:07 +0300

    tt
    ttt

--- src/test/java/test/test/rrrTest.java
+++ src/test/java/test/test/rrrTest.java
@@ -12,23 +12,21 @@ public class rrrTest {
 	    System.out.println("HELLLLLLLLLooooo");
 	    System.out.println("2туч€евшращдвырадр");
 	    try {
-			testGoogle();
+	    	Thread.sleep(10000);
+			//testGoogle();
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 	
-	
-
-	
-	public void testGoogle() throws InterruptedException {
+	public void testGoogle() {
 			
 		Selenium mSelenium = new DefaultSelenium ("localhost",4444,"*firefox","http://www.nngroup.com/articles/checkboxes-vs-radio-buttons");
 		mSelenium.start();
 		mSelenium.setTimeout("90000");
 		mSelenium.open("http://www.nngroup.com/articles/checkboxes-vs-radio-buttons/");
-		Thread.sleep(10000);
+		
 		mSelenium.click("permission");
 		System.out.println(mSelenium.isTextPresent("May we send you updates using e-mail?"));
 	}
