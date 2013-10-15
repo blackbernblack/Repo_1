fe80057ab343f6f9d052551a0bf364824db28d4b
blackbernblack <hqyejbfe@sharklasers.com>
2013-10-15 16:30:15 +0300

    hdghdg
    dghdgh

--- src/test/java/test/test/rrrTest.java
+++ src/test/java/test/test/rrrTest.java
@@ -1,7 +1,15 @@
 package test.test;
 
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
+
+import PageObject.User;
 
 import com.thoughtworks.selenium.DefaultSelenium;
 import com.thoughtworks.selenium.Selenium;
@@ -11,11 +19,30 @@ public class rrrTest {
 	@Test
 	public void main() {
 	    System.out.println("HELLLLLLLLLooooo");
-	    testGoogle();
+	    registerUserTest();
 	    System.out.println("==End of test==");
 	    
 	  
 	}
+	
+	 public void registerUserTest() {
+			WebDriver driver = new FirefoxDriver();
+			driver.get("https://geolotto.co.uk/");
+
+			User user = User.createValidUser();
+			user.setPostCode("73321");
+			PageObject.HomePage homePage = new PageObject.HomePage(driver);
+		    homePage.clickRegister();
+			ScheduledExecutorService worker = Executors.newSingleThreadScheduledExecutor();
+				
+			
+			// (new WebDriverWait(driver, 10))
+			// .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//input[@name='firstNameText']")));
+
+			PageObject.RegistrationPage registrationPage = new PageObject.RegistrationPage(driver);
+			registrationPage.registerUserError(user);
+			
+	    }
 	
 	public void testGoogle() {
 			
